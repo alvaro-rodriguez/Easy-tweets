2cf1098283cbea6e96ed86415921723053bb53e4
Álvaro Rodríguez <dem13k27@gmail.com>
2016-05-12 10:43:05 +0200

    Prueba3

--- wsgi/mybottleapp.py
+++ wsgi/mybottleapp.py
@@ -1,8 +1,9 @@
 from bottle import route, default_app
+import os
+from bottle import TEMPLATE_PATH
+TEMPLATE_PATH.append(os.path.join(os.environ['OPENSHIFT_REPO_DIR'], 'wsgi/views/index.tpl')) 
 
-@route('/name/<name>')
-def nameindex(name='Stranger'):
-    return '<strong>Hello, %s!</strong>' % name
+
  
 @route('/')
 def index():
@@ -11,6 +12,6 @@ def index():
 # This must be added in order to do correct path lookups for the views
 import os
 from bottle import TEMPLATE_PATH
-TEMPLATE_PATH.append(os.path.join(os.environ['OPENSHIFT_REPO_DIR'], 'wsgi/views/index.tpl')) 
+TEMPLATE_PATH.append(os.path.join(os.environ['OPENSHIFT_REPO_DIR'], 'wsgi/views/')) 
 
 application=default_app()
