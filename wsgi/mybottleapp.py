9375d6dec1e67ea16237d4327225617f44b064f1
Álvaro-Rodríguez <dem13k27@gmail.com>
2016-05-25 20:11:54 +0200

    prueba de seguidores 2.3

--- wsgi/mybottleapp.py
+++ wsgi/mybottleapp.py
@@ -185,7 +185,7 @@ def seguidores():
     r = requests.get(url=url,auth=oauth)
     if r.status_code == 200:
         doc=r.json()
-        return  template('seguidores.tpl' ,doc)
+        return  template('seguidores.tpl',doc=doc)
     else:
         doc=r.json()
         return doc
