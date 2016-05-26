d86718c4d6fcd7d21074b9af323f0727e4cb69ba
Álvaro-Rodríguez <dem13k27@gmail.com>
2016-05-26 17:40:11 +0200

    prueba seguidores 1.8.504

--- wsgi/mybottleapp.py
+++ wsgi/mybottleapp.py
@@ -261,8 +261,7 @@ def seguidores():
                    resource_owner_secret=TOKENS["access_token_secret"])
     
     url='https://api.twitter.com/1.1/statuses/retweets_of_me.json?count=500&amp;since_id=259320959964680190&amp;max_id=259320959964680500'
-    r = requests.get(url=url)
-		     #, auth=oauth)
+    r = requests.get(url=url, auth=oauth)
     if r.status_code == 200:
         doc=r.json()
         return doc
