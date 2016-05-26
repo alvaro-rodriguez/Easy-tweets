d1d07f604a5d6f8086891966ebfcbe66acdf6f8a
Álvaro-Rodríguez <dem13k27@gmail.com>
2016-05-26 18:47:56 +0200

    Añadida la plantilla de mensajes 1.8.511f

--- wsgi/mybottleapp.py
+++ wsgi/mybottleapp.py
@@ -155,7 +155,7 @@ def mensajes():
                       auth=oauth)
     doc=r 
     #return template('mensajes.tpl',doc=doc) 
-    return doc['0']['id']
+    return doc['0']
 """if r.status_code == 200:
         doc=r.json()
         return doc
