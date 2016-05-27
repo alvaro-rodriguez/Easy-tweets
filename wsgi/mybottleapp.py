3c9b0ff55bb7925feb59975dbb61b91d32e3feea
Álvaro Rodríguez <dem13k27@gmail.com>
2016-05-27 09:30:09 +0200

    prueba mensajes 9

--- wsgi/mybottleapp.py
+++ wsgi/mybottleapp.py
@@ -177,10 +177,11 @@ def mensajes():
                    resource_owner_key=TOKENS["access_token"],
                    resource_owner_secret=TOKENS["access_token_secret"])
     url='https://api.twitter.com/1.1/direct_messages.json?count=90'
-#    r = requests.get(url=url,
-#                      #data={"screen_name":'gatoapacheboina','count':'2'},
-#                      auth=oauth)
-#    doc=json.loads(r.text)
+    r = requests.get(url=url,
+                      #data={"screen_name":'gatoapacheboina','count':'2'},
+                      auth=oauth)
+    return str(r.status_code)
+    doc=json.loads(r.text)
     #return template('mensajes.tpl',doc=doc) 
 #    return doc
     return url
