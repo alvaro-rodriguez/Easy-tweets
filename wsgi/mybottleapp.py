ed5aad12dd11a6e54f429eb73c373de5e28c67f4
Álvaro Rodríguez <dem13k27@gmail.com>
2016-06-13 13:19:53 +0200

    Editados los formularios

--- wsgi/mybottleapp.py
+++ wsgi/mybottleapp.py
@@ -316,7 +316,7 @@ def seguidores():
     r = requests.get(url=url, auth=oauth)
     if r.status_code == 200:
         #doc=r.json()
-        return doc
+        return r
         #return template('seguidores.tpl',doc=doc)
     else:
         #doc=r.json()
